80c21fea7400b82fb9e9cc2d2f50dea0c0f1ef4d
maviharjit <mavi.harjit@gmail.com>
2020-12-02 19:28:16 -0700

    Added example of Vertical bias layout and chaining layout. learnt how manifest invokes the first activity and how activity ties into layout file. learnt how to add comments into xml files.

---
+++ Constraint Layout Example/src/main/java/com/example/constraintlayoutexample/ChainingActivity.java
@@ -0,0 +1,14 @@
+package com.example.constraintlayoutexample;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChainingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chaining);
+    }
+}
